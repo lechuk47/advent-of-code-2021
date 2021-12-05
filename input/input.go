d89e55777db0e694b9d57ffbd590bde9a86b2ffe
Jordi Piriz <jpiriz@redhat.com>
2021-12-05 17:17:48 +0100

    Add input module

---
+++ input/input.go
@@ -0,0 +1,21 @@
+package input
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadFile(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	lines := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return lines, scanner.Err()
+}
