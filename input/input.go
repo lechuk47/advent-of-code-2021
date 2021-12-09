812c4b6027dcbc393b56815fbbbf7809169b8157
Jordi Piriz <jpiriz@redhat.com>
2021-12-09 13:35:07 +0100

    Add func to input

--- input/input.go
+++ input/input.go
@@ -3,6 +3,8 @@ package input
 import (
 	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadFile(path string) ([]string, error) {
@@ -18,4 +20,17 @@ func ReadFile(path string) ([]string, error) {
 		lines = append(lines, line)
 	}
 	return lines, scanner.Err()
+}
+
+func ConvertStringToSliceOfInts(input string, separator string) ([]int, error) {
+	ints := []int{}
+	elements := strings.Split(input, separator)
+	for _, el := range elements {
+		n, err := strconv.ParseInt(el, 10, 0)
+		if err != nil {
+			return nil, err
+		}
+		ints = append(ints, int(n))
+	}
+	return ints, nil
 }
